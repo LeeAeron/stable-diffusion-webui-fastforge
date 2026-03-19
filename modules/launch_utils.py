e86ee011aea950f81a4ede3fa75ce78a708ad44c
unknown <aeronliru@gmail.com>
2026-03-19 21:43:25 +0200

    v1.80-280cu128

--- modules/launch_utils.py
+++ modules/launch_utils.py
@@ -387,7 +387,8 @@ import subprocess
 def prepare_environment():
     torch_command = [
         "uv", "pip", "install",
-        "torch", "torchvision", "torchaudio", "xformers==0.0.31.post1",
+        "torch==2.8.0+cu128", "torchvision==0.23.0+cu128",
+        "xformers==0.0.32.post1",
         "--extra-index-url", "https://download.pytorch.org/whl/cu128",
         "--system"
     ]
@@ -399,7 +400,7 @@ def prepare_environment():
 
     requirements_file = os.environ.get('REQS_FILE', "requirements_versions.txt")
 
-    xformers_package = os.environ.get('XFORMERS_PACKAGE', 'xformers==0.0.31.post1')
+    xformers_package = os.environ.get('XFORMERS_PACKAGE', 'xformers==0.0.32.post1')
     clip_package = os.environ.get('CLIP_PACKAGE', "https://github.com/openai/CLIP/archive/d50d76daa670286dd6cacf3bcd80b5e4823fc8e1.zip")
     openclip_package = os.environ.get('OPENCLIP_PACKAGE', "https://github.com/mlfoundations/open_clip/archive/bb6e834e9c70d9c27d0dc3ecedeebeaeb1ffad6b.zip")
 
